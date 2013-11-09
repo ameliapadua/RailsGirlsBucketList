a78b9cf4f552ae16559207bdd4caba054dd73b5f
Amelia Padua <amelia.padua@gmail.com>
2013-11-09 15:58:43 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuckList::Application.config.secret_token = '2a7dab4c3433cd7f4502e89b8d35dfb560e66066a8b2d43f22d2e549bd7fca8309c8323983671e3943378079a68524ee322d185639919bd2b41e4223fe92ccc1'
